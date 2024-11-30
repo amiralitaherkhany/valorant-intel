5e6855fea5a7c55dbd05254b31c16f2920dee288
amiralitaherkhany <122179669+amiralitaherkhany@users.noreply.github.com>
2024-12-01 01:23:02 +0330

    added basic appbar to main wrapper and changet font size of bottom navigation bar

--- lib/core/presentation/main_wrapper.dart
+++ lib/core/presentation/main_wrapper.dart
@@ -9,42 +9,48 @@ class MainWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: navigationShell,
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          enableFeedback: true,
-          currentIndex: navigationShell.currentIndex,
-          onTap: (index) {
-            navigationShell.goBranch(
-              index,
-              initialLocation: index == navigationShell.currentIndex,
-            );
-          },
-          items: [
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.home),
-              label: AppLocalizations.of(context)!.home,
-            ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.collections),
-              label: AppLocalizations.of(context)!.collections,
-            ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.gamepad),
-              label: AppLocalizations.of(context)!.game,
-            ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.person),
-              label: AppLocalizations.of(context)!.profile,
-            ),
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.settings),
-              label: AppLocalizations.of(context)!.settings,
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        toolbarHeight: 0,
+        scrolledUnderElevation: 0,
+      ),
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 5,
+        selectedFontSize: 10,
+        unselectedFontSize: 9,
+        type: BottomNavigationBarType.fixed,
+        enableFeedback: true,
+        currentIndex: navigationShell.currentIndex,
+        onTap: (index) {
+          navigationShell.goBranch(
+            index,
+            initialLocation: index == navigationShell.currentIndex,
+          );
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            label: AppLocalizations.of(context)!.home,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.collections),
+            label: AppLocalizations.of(context)!.collections,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.gamepad),
+            label: AppLocalizations.of(context)!.game,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.person),
+            label: AppLocalizations.of(context)!.profile,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.settings),
+            label: AppLocalizations.of(context)!.settings,
+          ),
+        ],
       ),
     );
   }
