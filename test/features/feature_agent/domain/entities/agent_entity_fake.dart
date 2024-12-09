7a2f4c3d7609375a71b302a3908ed8fd2baae46e
amiralitaherkhany <122179669+amiralitaherkhany@users.noreply.github.com>
2024-12-09 16:01:17 +0330

    fake agent entity object creator for test purposes

---
+++ test/features/feature_agent/domain/entities/agent_entity_fake.dart
@@ -0,0 +1,41 @@
+import 'package:valorant_intel/features/feature_agent/data/models/agent_model/ability.dart';
+import 'package:valorant_intel/features/feature_agent/data/models/agent_model/role.dart';
+import 'package:valorant_intel/features/feature_agent/domain/entities/agent_entity.dart';
+
+AgentEntity createFakeAgentEntity() {
+  return AgentEntity(
+    uuid: '',
+    displayName: 'displayName',
+    description: 'description',
+    developerName: 'developerName',
+    characterTags: 'characterTags',
+    displayIcon: 'displayIcon',
+    displayIconSmall: 'displayIconSmall',
+    bustPortrait: 'bustPortrait',
+    fullPortrait: 'fullPortrait',
+    fullPortraitV2: 'fullPortraitV2',
+    killfeedPortrait: 'killfeedPortrait',
+    background: 'background',
+    backgroundGradientColors: ['', '', '', ''],
+    assetPath: 'assetPath',
+    isFullPortraitRightFacing: true,
+    isPlayableCharacter: true,
+    isAvailableForTest: true,
+    isBaseContent: true,
+    role: Role(
+      assetPath: '',
+      description: '',
+      displayIcon: '',
+      displayName: '',
+      uuid: '',
+    ),
+    recruitmentData: 'recruitmentData',
+    abilities: [
+      Ability(),
+      Ability(),
+      Ability(),
+      Ability(),
+    ],
+    voiceLine: 'voiceLine',
+  );
+}
