d859eddcf02007ceda7d85888a239010524dbf55
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-03-20 16:55:58 +0330

    fix(map_repository_impl): filter out specific map UUIDs from results
    The commit modifies the `getAllMaps` method to exclude maps with specific UUIDs ("5914d1e0-40c4-cfdd-6b88-eba06347686c" and "1f10dab3-4294-3827-fa35-c2aa00213cf3") from the returned list. This change ensures that these maps are not included in the results.

--- lib/features/feature_map/data/repositories/map_repository_impl.dart
+++ lib/features/feature_map/data/repositories/map_repository_impl.dart
@@ -14,7 +14,14 @@ class MapRepositoryImpl implements MapRepository {
   Future<Either<String, List<GameMap>>> getAllMaps() async {
     try {
       return await _mapDatasource.getAllMaps().then(
-            (mapList) => right(mapList),
+            (mapList) => right(mapList
+                .where(
+                  (element) => ![
+                    "5914d1e0-40c4-cfdd-6b88-eba06347686c",
+                    "1f10dab3-4294-3827-fa35-c2aa00213cf3"
+                  ].contains(element.uuid),
+                )
+                .toList()),
           );
     } on ApiException catch (exception) {
       return left(exception.message);
