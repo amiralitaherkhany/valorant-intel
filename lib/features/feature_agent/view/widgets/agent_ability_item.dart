2f03210fdba0f3bc69cab3e10306ef797c3eb8ef
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-09-04 18:32:12 +0330

    refactor(agent_ability_item): replace InkWell with GestureDetector for better control
    GestureDetector provides more flexibility for handling touch events compared to InkWell, which is primarily designed for Material Design ink ripple effects.

--- lib/features/feature_agent/view/widgets/agent_ability_item.dart
+++ lib/features/feature_agent/view/widgets/agent_ability_item.dart
@@ -11,7 +11,7 @@ class AgentAbilityItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return GestureDetector(
       onTap: () {
         showModalBottomSheet(
           context: context,
