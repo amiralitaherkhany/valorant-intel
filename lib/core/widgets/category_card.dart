200ae5f0c4e1c8d567fc8796caf17e3295458680
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-09-04 18:29:37 +0330

    refactor(widgets): replace InkWell with GestureDetector for better touch handling

--- lib/core/widgets/category_card.dart
+++ lib/core/widgets/category_card.dart
@@ -14,7 +14,7 @@ class CategoryCard extends StatelessWidget {
   final String title;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return GestureDetector(
       onTap: onTap,
       child: Container(
         padding: const EdgeInsets.all(0),
