c90545ea728f828a7b409190659cc827f6bff54d
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-09-20 15:58:33 +0330

    refactor(weapon): move WeaponAppBar implementation to a separate widget for improved modularity and readability

--- lib/features/feature_weapon/view/pages/weapon_detail_page.dart
+++ lib/features/feature_weapon/view/pages/weapon_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:valorant_intel/config/theme/app_color_scheme.dart';
 import 'package:valorant_intel/features/feature_weapon/data/models/weapon.dart';
+import 'package:valorant_intel/features/feature_weapon/view/widgets/weapon_app_bar.dart';
 
 class WeaponDetailPage extends StatelessWidget {
   const WeaponDetailPage({super.key, required this.weapon});
@@ -14,7 +15,10 @@ class WeaponDetailPage extends StatelessWidget {
       body: CustomScrollView(
         physics: const BouncingScrollPhysics(),
         slivers: [
-          WeaponAppBar(weapon: weapon),
+          WeaponAppBar(
+            imageUrl: weapon.displayIcon ?? "",
+            title: weapon.displayName ?? "",
+          ),
           SliverPadding(
             padding: EdgeInsetsGeometry.all(10),
             sliver: SliverToBoxAdapter(
@@ -69,68 +73,6 @@ class WeaponDetailPage extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class WeaponAppBar extends StatelessWidget {
-  const WeaponAppBar({
-    super.key,
-    required this.weapon,
-  });
-
-  final Weapon weapon;
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverAppBar(
-      flexibleSpace: FlexibleSpaceBar(
-        titlePadding: EdgeInsets.all(6),
-        title: FittedBox(
-          fit: BoxFit.scaleDown,
-          child: Text(
-            weapon.displayName ?? "",
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.displayLarge?.copyWith(
-              fontSize: 35,
-            ),
-          ),
-        ),
-        background: Container(
-          padding: EdgeInsets.only(
-            top: MediaQuery.viewPaddingOf(context).top,
-            bottom: 30,
-          ),
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color(0xFF3E5151),
-                Color(0xFFDECBA4),
-              ],
-              begin: AlignmentDirectional.topStart,
-              end: AlignmentDirectional.bottomEnd,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(30),
-            child: CachedNetworkImage(
-              imageUrl: weapon.displayIcon ?? "",
-              fit: BoxFit.contain,
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-            ),
-          ),
-        ),
-        centerTitle: true,
-      ),
-      elevation: 0,
-      scrolledUnderElevation: 0,
-      pinned: true,
-      floating: false,
-      snap: false,
-      stretch: true,
-      expandedHeight: 200,
     );
   }
 }
---
+++ lib/features/feature_weapon/view/widgets/weapon_app_bar.dart
@@ -0,0 +1,65 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class WeaponAppBar extends StatelessWidget {
+  const WeaponAppBar({
+    super.key,
+    required this.title,
+    required this.imageUrl,
+  });
+
+  final String title;
+  final String imageUrl;
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: EdgeInsets.all(6),
+        title: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.displayLarge?.copyWith(
+              fontSize: 35,
+            ),
+          ),
+        ),
+        background: Container(
+          padding: EdgeInsets.only(
+            top: MediaQuery.viewPaddingOf(context).top,
+            bottom: 30,
+          ),
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF3E5151),
+                Color(0xFFDECBA4),
+              ],
+              begin: AlignmentDirectional.topStart,
+              end: AlignmentDirectional.bottomEnd,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: CachedNetworkImage(
+              imageUrl: imageUrl,
+              fit: BoxFit.contain,
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      pinned: true,
+      floating: false,
+      snap: false,
+      stretch: true,
+      expandedHeight: 200,
+    );
+  }
+}
