f1fae13afa1a8cf2c64edae8611307e75eca4901
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-09-20 18:09:34 +0330

    ✨ feat(weapon): implement weapon skin viewer in detail page
    - add WeaponSkinViewer widget to display weapon skins
    - display skin images using CachedNetworkImage with Shimmer placeholder
    - add horizontal ListView to showcase available skins
    - display skin names below corresponding images

--- lib/features/feature_weapon/view/pages/weapon_detail_page.dart
+++ lib/features/feature_weapon/view/pages/weapon_detail_page.dart
@@ -65,10 +65,126 @@ class WeaponDetailPage extends StatelessWidget {
           const SliverToBoxAdapter(
             child: Divider(indent: 20, endIndent: 20),
           ),
+          WeaponSkinViewer(weapon: weapon),
           SliverList(
             delegate: SliverChildBuilderDelegate(
               childCount: 50,
               (context, index) => Text("1"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class WeaponSkinViewer extends StatelessWidget {
+  const WeaponSkinViewer({
+    super.key,
+    required this.weapon,
+  });
+
+  final Weapon weapon;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          CachedNetworkImage(
+            imageUrl: weapon.skins?.first.displayIcon ?? "",
+            height: 200,
+            placeholder: (context, url) {
+              return Shimmer.fromColors(
+                baseColor: AppColorScheme.grey,
+                highlightColor: AppColorScheme.white,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              );
+            },
+            errorWidget: (context, url, error) => const Icon(
+              Icons.image_not_supported,
+              size: 50,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: 140,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: weapon.skins?.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 250,
+                    decoration: BoxDecoration(
+                      color: AppColorScheme.white,
+                      borderRadius: BorderRadius.circular(7),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 5,
+                        vertical: 3,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: CachedNetworkImage(
+                              imageUrl: weapon.skins?[index].displayIcon ?? "",
+                              placeholder: (context, url) {
+                                return Shimmer.fromColors(
+                                  baseColor: AppColorScheme.grey,
+                                  highlightColor: AppColorScheme.white,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
+                                );
+                              },
+                              errorWidget: (context, url, error) => const Icon(
+                                Icons.image_not_supported,
+                                size: 50,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: FittedBox(
+                              fit: BoxFit.scaleDown,
+                              child: Text(
+                                weapon.skins?[index].displayName ?? "",
+                                textAlign: TextAlign.center,
+                                style:
+                                    Theme.of(
+                                      context,
+                                    ).textTheme.displayLarge?.copyWith(
+                                      fontSize: 27,
+                                      color: Colors.black,
+                                    ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ],
